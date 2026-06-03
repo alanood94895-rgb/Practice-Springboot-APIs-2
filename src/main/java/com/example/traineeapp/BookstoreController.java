8ab586ba6698fec6e53798a418fa453ae25879b2
CompanyProfile <alanood94895@gmail.com>
2026-06-03 14:41:55 +0400

    Add BookstoreController Class

--- src/main/java/com/example/traineeapp/BookstoreController.java
+++ src/main/java/com/example/traineeapp/BookstoreController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class BookstoreController {
     private static List<InventoryBook> inventory = new ArrayList<>();
-
+    private List<InventoryBook> books = new ArrayList<>();
 
     @GetMapping("/addInventoryBook")
     public String addInventoryBook(
@@ -23,5 +23,26 @@ public class BookstoreController {
         return "Book added to inventory successfully!";
     }
 
-}
+    @GetMapping("/checkStock")
+    public String checkStock(@RequestParam int id) {
 
+        for (InventoryBook book : books) {
+
+            if (book.getBookId() == id) {
+
+                if (book.getStock() > 0) {
+                    return "Book Available! Title: "
+                            + book.getTitle()
+                            + ", Price: $"
+                            + book.getPrice();
+                } else {
+                    return "Sorry, the book '"
+                            + book.getTitle()
+                            + "' is sold out.";
+                }
+            }
+        }
+
+        return "The bookstore does not carry a book with ID " + id;
+    }
+}
