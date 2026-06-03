35fddb14f0adf5b87b457960fcd870692247aaab
CompanyProfile <alanood94895@gmail.com>
2026-06-03 09:27:34 +0400

    Creating Book Blueprint

---
+++ src/main/java/com/example/traineeapp/TraineeappApplication.java
@@ -0,0 +1,13 @@
+package com.example.traineeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineeappApplication.class, args);
+	}
+
+}
