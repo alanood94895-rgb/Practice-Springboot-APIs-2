e2f7208ff2a40823a3b8b754ada1363fa9d85060
CompanyProfile <alanood94895@gmail.com>
2026-06-03 11:43:25 +0400

    Add method for LibraryController Class

--- src/main/java/com/example/traineeapp/BookController.java
+++ src/main/java/com/example/traineeapp/BookController.java
@@ -86,6 +86,7 @@ public class BookController {
 
         return "Error: Author ID " + authorId + " does not exist. Book not added.";
     }
+
 }
 
 
--- src/main/java/com/example/traineeapp/LibraryController.java
+++ src/main/java/com/example/traineeapp/LibraryController.java
@@ -29,5 +29,46 @@ public class LibraryController {
     public List<Author> getAllAuthors() {
         return authorList;
     }
+    @GetMapping("/author-report")
+    public String authorReport(
+            @RequestParam String authorName) {
+
+        Author foundAuthor = null;
+
+        for (Author a : authorList) {
+            if (a.getName().equalsIgnoreCase(authorName)) {
+                foundAuthor = a;
+                break;
+            }
+        }
+
+        if (foundAuthor == null) {
+            return "Error: Author '" + authorName + "' not found.";
+        }
+
+        StringBuilder booksWritten = new StringBuilder();
+        boolean hasBooks = false;
+
+        for (Book b : bookList) {
+            if (b.getAuthorId() == foundAuthor.getId()) {
+                booksWritten.append("").append(b.getName()).append("\n");
+                hasBooks = true;
+            }
+        }
+
+        StringBuilder report = new StringBuilder();
+        report.append("Author Report\n");
+        report.append("Name: ").append(foundAuthor.getName()).append("\n");
+        report.append("Biography: ").append(foundAuthor.getBiography()).append("\n");
+        report.append("Books Written:\n");
+
+        if (hasBooks) {
+            report.append(booksWritten);
+        } else {
+            report.append("None");
+        }
+
+        return report.toString();
+    }
 }
 
