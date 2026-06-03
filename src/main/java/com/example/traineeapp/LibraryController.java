2f287f7172f804f40b95d41d6c6095e94a5700af
CompanyProfile <alanood94895@gmail.com>
2026-06-03 11:32:18 +0400

    Add method for LibraryController Class

--- src/main/java/com/example/traineeapp/LibraryController.java
+++ src/main/java/com/example/traineeapp/LibraryController.java
@@ -1,4 +1,33 @@
 package com.example.traineeapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+
 public class LibraryController {
+    private static List<Author> authorList = new ArrayList<>();
+    private static List<Book> bookList = new ArrayList<>();
+
+    @GetMapping("/add-author")
+    public String addAuthor(
+            @RequestParam int id,
+            @RequestParam String name,
+            @RequestParam String biography) {
+
+        Author author = new Author(id, name, biography);
+        authorList.add(author);
+
+        return "Author added successfully!";
+    }
+
+    @GetMapping("/all-authors")
+    public List<Author> getAllAuthors() {
+        return authorList;
+    }
 }
+
