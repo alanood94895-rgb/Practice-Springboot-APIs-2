3a59337c4ec2aae2c1cf32e6bf9c724ed25b7a43
CompanyProfile <alanood94895@gmail.com>
2026-06-03 14:23:35 +0400

    add inventory book

--- src/main/java/com/example/traineeapp/BookstoreController.java
+++ src/main/java/com/example/traineeapp/BookstoreController.java
@@ -1,4 +1,27 @@
 package com.example.traineeapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookstoreController {
+    private static List<InventoryBook> inventory = new ArrayList<>();
+
+
+    @GetMapping("/add-inventory-book")
+    public String addInventoryBook(
+            @RequestParam int bookId,
+            @RequestParam String title,
+            @RequestParam double price,
+            @RequestParam int stock) {
+
+        InventoryBook book = new InventoryBook(bookId, title, price, stock);
+        BookstoreController.inventory.add(book);
+
+        return "Book added to inventory successfully!";
+    }
+
 }
+
